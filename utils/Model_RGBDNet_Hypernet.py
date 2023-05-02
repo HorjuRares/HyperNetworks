9295232436bf17beec52f2dcdf2afc5ae6b042f4
Horju <Rares.Horju@elektrobit.com>
2023-05-02 21:33:27 +0300

    Update Model_RGBDNet_Hypernet.py

--- utils/Model_RGBDNet_Hypernet.py
+++ utils/Model_RGBDNet_Hypernet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 from hypernetwork_modules import HyperNetwork, Embedding
 from ResNetFunctional import ResnetForHypernet
@@ -12,6 +13,10 @@ class Model_RGBDNet_Hypernet(nn.Module):
 
         self.Hypernet = HyperNetwork()
 
+        model_params = filter(lambda p: p.requires_grad, self.Hypernet.parameters())
+        model_params = sum(np.prod(p.size()) for p in model_params)
+        print('Hypernet params', model_params)
+
         self.embeddings_sizes = [[4, 4], [4, 4], [4, 4], [4, 4], [8, 4], [8, 8], [8, 8], [8, 8],
                                  [16, 8], [16, 16], [16, 16], [16, 16], [32, 16], [32, 32], [32, 32], [32, 32]]
         self.depth_embeddings_list = nn.ModuleList()
@@ -21,10 +26,22 @@ class Model_RGBDNet_Hypernet(nn.Module):
             self.depth_embeddings_list.append(Embedding(z_num=self.embeddings_sizes[emb_idx], z_dim=64))
             self.rgb_embeddings_list.append(Embedding(z_num=self.embeddings_sizes[emb_idx], z_dim=64))
 
+        model_params = filter(lambda p: p.requires_grad, self.depth_embeddings_list.parameters())
+        model_params = sum(np.prod(p.size()) for p in model_params)
+        print('Depth embeddings params', model_params)
+
         self.depth_backbone = ResnetForHypernet(in_channels=1, cached_layers=True)
         self.rgb_backbone = ResnetForHypernet(in_channels=3, cached_layers=True)
 
+        model_params = filter(lambda p: p.requires_grad, self.depth_backbone.parameters())
+        model_params = sum(np.prod(p.size()) for p in model_params)
+        print('Depth backbone params', model_params)
+
         self.decoding_head = Model_Decoder_Head(num_classes=num_classes)
+
+        model_params = filter(lambda p: p.requires_grad, self.decoding_head.parameters())
+        model_params = sum(np.prod(p.size()) for p in model_params)
+        print('Decoding head params', model_params)
 
     def forward(self, x: torch.Tensor):
         x_depth = self.depth_backbone(x=x[:, :1, :, :],
@@ -44,6 +61,11 @@ class Model_RGBDNet_Hypernet(nn.Module):
 
 def main():
     model = Model_RGBDNet_Hypernet().to('cuda')
+
+    model_params = filter(lambda p: p.requires_grad, model.parameters())
+    model_params = sum(np.prod(p.size()) for p in model_params)
+    print(model_params)
+
     x = torch.randn(size=(1, 4, 320, 320)).to('cuda')
     y = model(x)
     print(y.shape)
